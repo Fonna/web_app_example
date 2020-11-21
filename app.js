2fd1a00834fa8c8d8ddc7cc370fbd3fe55dc5a77
dish <jiangpeng@cloudloan.com>
2020-11-21 10:21:29 +0800

    Test moving console message

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 'use strict'
 
-console.log('this\'s a hidden message!');
+let today = new Date()
+let formatDate = today.toDateString()
+let selectElement = document.getElementById('date')
+selectElement.innerHTML = formatDate
 
-// This is a single-line comment.
-
-/* Here's a
-multi-line
-comment. */
-
-// console.log('This code is no longer active')
-
-let today = new Date();
-let formatDate = today.toDateString();
-let selectElement = document.getElementById('date');
-selectElement.innerHTML = formatDate;
+console.log('Here\'s a hidden message')
